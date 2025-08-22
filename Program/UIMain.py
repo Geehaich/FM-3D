a321aafbf8de8dbcf8b4725763faeb82ed66805b
gottl <gottl@maf-france.com>
2025-08-22 16:03:42 +0200

    a

--- Program/UIMain.py
+++ Program/UIMain.py
@@ -1,4 +1,6 @@
-from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QPushButton, QGridLayout, QHBoxLayout
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QIntValidator
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QPushButton, QGridLayout, QHBoxLayout, QLineEdit
 import PyQt6.QtWidgets as pqw
 from Step_controller import PrintController
 
@@ -12,24 +14,56 @@ class TestUI(QWidget):
         super().__init__()
 
         self.con_ref = print_controller
-        self.lay = QGridLayout()
+        self.lay = QVBoxLayout()
+
+        hlx = QHBoxLayout() #group of controls for the X axis. buttons for mvt in either direction, QLine edit for speed in rpm
         self.but_Xplus = QPushButton("X+")
+        self.but_Xplus.setFixedSize(60, 30)
         self.but_Xminus = QPushButton("X-")
+        self.but_Xminus.setFixedSize(60, 30)
+        self.qlin_x_spd = QLineEdit("60")
+        self.qlin_x_spd.setValidator(QIntValidator(1, 100))
+        self.qlin_x_spd.setFixedSize(60, 30)
+
+        hlx.addWidget(QLabel("X")) #add to layout
+        hlx.addWidget(self.but_Xplus)
+        hlx.addWidget(self.but_Xminus)
+        hlx.addWidget(self.qlin_x_spd)
+
+        self.lay.addLayout(hlx)
+
+        hle = QHBoxLayout() #same for E (extruder) axis
         self.but_Eplus = QPushButton("E+")
         self.but_Eminus = QPushButton("E-")
+        self.but_Eplus.setFixedSize(60,30)
+        self.but_Eminus.setFixedSize(60,30)
+        self.qlin_e_spd = QLineEdit("40")
+        self.qlin_e_spd.setValidator(QIntValidator(1,100))
+        self.qlin_e_spd.setFixedSize(60, 30)
 
-        if self.con_ref !=None:
-            self.but_Xplus.pressed.connect(lambda : self.con_ref.x_motor.move_rpm(60))
+        hle.addWidget(QLabel("E"))
+        hle.addWidget(self.but_Eplus)
+        hle.addWidget(self.but_Eminus)
+        hle.addWidget(self.qlin_e_spd)
+        self.lay.addLayout(hle)
+
+        self.print_but = QPushButton('PRINT')
+        self.print_but.setFixedSize(150,50)
+        self.lay.addWidget(self.print_but,alignment=Qt.AlignmentFlag.AlignCenter)
+
+        if self.con_ref is not None:
+            self.but_Xplus.pressed.connect(lambda : self.con_ref.x_motor.move_rpm(int(self.qlin_x_spd.text())))
             self.but_Xplus.released.connect(lambda : self.con_ref.x_motor.stop())
-            self.but_Xminus.pressed.connect(lambda : self.con_ref.x_motor.move_rpm(-60))
+            self.but_Xminus.pressed.connect(lambda : self.con_ref.x_motor.move_rpm(-int(self.qlin_x_spd.text())))
             self.but_Xminus.released.connect(lambda : self.con_ref.x_motor.stop())
-            self.but_Eplus.pressed.connect(lambda: self.con_ref.print_head.e_motor.move_rpm(40))
+            self.but_Eplus.pressed.connect(lambda: self.con_ref.print_head.e_motor.move_rpm(int(self.qlin_e_spd.text())))
             self.but_Eplus.released.connect(lambda: self.con_ref.print_head.e_motor.stop())
-            self.but_Eminus.pressed.connect(lambda: self.con_ref.print_head.e_motor.move_rpm(-40))
+            self.but_Eminus.pressed.connect(lambda: self.con_ref.print_head.e_motor.move_rpm(-int(self.qlin_e_spd.text())))
             self.but_Eminus.released.connect(lambda: self.con_ref.print_head.e_motor.stop())
 
 
         radio_lay = QHBoxLayout()
+        radio_lay.addWidget(QLabel("Laser"))
         self.rad_las_off = pqw.QRadioButton("OFF")
         self.rad_las_oneperc = pqw.QRadioButton("1%")
         self.rad_las_on = pqw.QRadioButton("ON")
@@ -40,17 +74,11 @@ class TestUI(QWidget):
         radio_lay.addWidget(self.rad_las_off)
         radio_lay.addWidget(self.rad_las_oneperc)
         radio_lay.addWidget(self.rad_las_on)
-        self.rad_las_off.click()
+
+        self.lay.addLayout(radio_lay)
+        #self.rad_las_off.click()
 
 
-        self.lay.addWidget(QLabel("X axis"),0,0)
-        self.lay.addWidget(self.but_Xplus,0,1)
-        self.lay.addWidget(self.but_Xminus,0,2)
-        self.lay.addWidget(QLabel("E axis"),1,0)
-        self.lay.addWidget(self.but_Eplus,1,1)
-        self.lay.addWidget(self.but_Eminus,1,2)
-        self.lay.addWidget(QLabel("Laser"),2,0)
-        self.lay.addLayout(radio_lay,2,1)
 
         self.setLayout(self.lay)
 
@@ -63,12 +91,21 @@ class TestUI(QWidget):
         if self.rad_las_off.isChecked():
             self.con_ref.print_head.stop_laser()
 
+    def print_move(self):
+
+        self.rad_las_on.click()
+        self.con_ref.x_motor.move_rpm(int(self.qlin_x_spd.text()))
+        self.con_ref.print_head.e_motor.move_rpm(int(self.qlin_e_spd.text()))
+
+    def print_stop(self):
+        self.rad_las_off.click()
+        self.con_ref.x_motor.stop()
+        self.con_ref.print_head.stop()
 
 
-conf_X = StepperConfig("X",3,4,800,10)
-conf_E = StepperConfig("E",17,27,800,4)
-print(conf_E)
-prin_conf = PrintController(conf_X,None,None,None,conf_E,21)
+#conf_X = StepperConfig("X",3,4,800,10)
+#conf_E = StepperConfig("E",17,27,800,4)
+#prin_conf = PrintController(conf_X,None,None,None,conf_E,21)
 
 # You need one (and only one) QApplication instance per application.
 # Pass in sys.argv to allow command line arguments for your app.
@@ -76,7 +113,7 @@ prin_conf = PrintController(conf_X,None,None,None,conf_E,21)
 app = QApplication(sys.argv)
 
 # Create a Qt widget, which will be our window.
-Testwid = TestUI(prin_conf)
+Testwid = TestUI(None)
 Testwid.show()  # IMPORTANT!!!!! Windows are hidden by default.
 
 # Start the event loop.
